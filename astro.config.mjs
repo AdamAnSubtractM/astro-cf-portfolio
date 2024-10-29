ed9efc227f9176dfeea1c00adc57e6fff21ad1db
Adam Knee <adam.l.knee@gmail.com>
2024-10-28 23:04:29 -0400

    Add playwright for headless screenshotting of a resume.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import deno from "@astrojs/deno";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  adapter: deno({
+    start: false,
+  }),
+  integrations: [mdx(), sitemap()],
+  output: "hybrid",
+  site: "https://adamknee.dev",
 });
