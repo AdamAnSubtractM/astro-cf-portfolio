1530a3e32dc6ae5f721041056a86b40a866b8ff4
Adam Knee <adam.l.knee@gmail.com>
2025-05-18 23:14:16 -0400

    Further tweaks to Contact page.

--- src/components/solid/ContactCard.tsx
+++ src/components/solid/ContactCard.tsx
@@ -1,6 +1,8 @@
 import { Toaster, toast } from 'solid-toast';
 import Button from './Button';
 import { mergeProps, type Component } from 'solid-js';
+import GithubSVG from './GithubSVG';
+import LinkedInSVG from './LinkedInSVG';
 
 import styles from './style-modules/ContactCard.module.css';
 
@@ -20,6 +22,32 @@ const ContactCard: Component<{ email?: string }> = (props) => {
         <input type="email" value={merged.email} readOnly />
         <Button onClick={copyEmail}>Copy Email</Button>
       </fieldset>
+      <section class={styles.social}>
+        <a
+          class="icon-button icon-button--large"
+          href="https://github.com/AdamAnSubtractM"
+          target="_blank"
+          title="View Adam's GitHub profile"
+        >
+          <GithubSVG />
+        </a>
+        <a href="https://github.com/AdamAnSubtractM" target="_blank" title="View Adam's GitHub profile">
+          Follow me on Github
+        </a>
+      </section>
+      <section class={styles.social}>
+        <a
+          class="icon-button icon-button--large"
+          href="https://www.linkedin.com/in/adam-knee-web-dev/"
+          target="_blank"
+          title="View Adam's LinkedIn profile"
+        >
+          <LinkedInSVG />
+        </a>
+        <a href="https://www.linkedin.com/in/adam-knee-web-dev/" target="_blank" title="View Adam's LinkedIn profile">
+          Connect with me on LinkedIn
+        </a>
+      </section>
     </>
   );
 };
